52e338e4e0de85732b632e106c3536a1a1fdd678
Alexandre Perché <66827361+vegasword@users.noreply.github.com>
2026-04-02 14:40:40 +0200

    Cleaned up imports with wildcards

--- composeApp/src/commonMain/kotlin/com/codingfactory/relaunch/ui/onboarding/BlobsDecoration.kt
+++ composeApp/src/commonMain/kotlin/com/codingfactory/relaunch/ui/onboarding/BlobsDecoration.kt
@@ -7,10 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.*
 
 @Composable
 fun BlobsDecoration(modifier: Modifier = Modifier) {
--- composeApp/src/commonMain/kotlin/com/codingfactory/relaunch/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/codingfactory/relaunch/ui/theme/Theme.kt
@@ -1,7 +1,6 @@
 package com.codingfactory.relaunch.ui.theme
 
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.*
 
 val OrangeStart = Color(0xFFFF8C00)
 val RedEnd = Color(0xFFFF1A1A)
